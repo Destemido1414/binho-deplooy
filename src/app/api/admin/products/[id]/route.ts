9399045363bebeab0416c092628aaecfdf7c992c
unknown <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>
2026-03-04 14:40:23 -0300

    fix api admin products id route

--- src/app/api/admin/products/[id]/route.ts
+++ src/app/api/admin/products/[id]/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    message: "Lista de produtos"
-  })
+    message: "API products funcionando"
+  });
 }
 
 export async function POST() {
   return NextResponse.json({
     message: "Produto criado"
-  })
+  });
 }
